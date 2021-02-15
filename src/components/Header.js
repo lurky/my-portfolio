645e7ac4e065afdd64476ffc6aff2bbb2763909e
Michael <mlwood952@gmail.com>
2021-02-15 09:11:26 -0500

    Added Experience section with react-vertical-timeline, added Typed function in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import Typed from 'react-typed';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const Header = () => {
     return (
         <div className="bg-header">
-            <div className="container">   
+            <div className="container">
                 <div className="main-info">
-                    <h1>Softwatre Developer</h1>
+                    <h1>Software Developer</h1>
+                    <Typed 
+                
+                    strings={["Hi, I'm Michael Wood", "Hi, I'm motivated","Hi, I'm passionate", "Hi, I'm ready to get started"]}  
+                    typeSpeed={60}
+                    backSpeed={60}
+                    loop /> 
                     <button type="button" class="btn btn-outline-warning btn-lg" rel="contact.js">Contact</button>
                 </div>
             </div>
