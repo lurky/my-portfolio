f373b0173722716a5653a135557796bfe9c704d3
Michael <mlwood952@gmail.com>
2021-02-14 21:54:02 -0500

    Initial Commit for Repo creation

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const Header = () => {
+    return (
+        <div className="bg-header">
+            <div className="container">   
+                <div className="main-info">
+                    <h1>Softwatre Developer</h1>
+                    <button type="button" class="btn btn-outline-warning btn-lg" rel="contact.js">Contact</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
